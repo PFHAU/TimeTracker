127f77e6e3dc84b12cb1f46f4bc7ed5fff7e22ef
unknown <hakim.mp7@gmail.com>
2020-05-25 19:27:37 +0200

    export

--- src/app/export/export.component.ts
+++ src/app/export/export.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { TaskService } from '../service/task.service';
+import { Task } from '../model/Task.model';
+import { LocalStorageService } from '../service/local-storage.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,25 +13,69 @@ import { Router } from '@angular/router';
 export class ExportComponent implements OnInit {
 
   exportForm: FormGroup;
+  submitted = false;
+
   constructor(private formBuilder: FormBuilder,
-              private taskService: TaskService,
-              private router: Router
+              private router: Router,
+              private localStorageService: LocalStorageService
     ) { }
 
   ngOnInit() {
     this.initForm();
   }
 
+  get f() { return this.exportForm.controls; }
+
   initForm(){
     this.exportForm = this.formBuilder.group({
-      eMail:['', [Validators.required,Validators.email]],
+      email:['', [Validators.required,Validators.email]],
+      name:['',Validators.required],
     })
-    alert("yo1");
   }
 
   onSubmitForm(){
-    this.router.navigate(['/overview']);
-    alert("yo");
+
+    this.submitted = true;
+    if (this.exportForm.invalid) {
+      return;
+    }
+    
+    const separator = ',';
+    const keys = Object.keys(this.localStorageService.tasks[0]);
+    const csvContent =
+      keys.join(separator) +
+      '\n' +
+      this.localStorageService.tasks.map(row => {
+        return keys.map(k => {
+          let cell = row[k] === null || row[k] === undefined ? '' : row[k];
+          cell = cell instanceof Date
+            ? cell.toLocaleString()
+            : cell.toString().replace(/"/g, '""');
+          if (cell.search(/("|,|\n)/g) >= 0) {
+            cell = `"${cell}"`;
+          }
+          return cell;
+        }).join(separator);
+      }).join('\n'); 
+
+
+
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+      navigator.msSaveBlob(blob, "tasks.csv");
+    } else {
+      const link = document.createElement('a');
+      if (link.download !== undefined) {
+        // Browsers that support HTML5 download attribute
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', "tasks.csv");
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
   }
 
 }
